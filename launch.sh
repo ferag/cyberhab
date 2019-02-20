16209c576d616ed4fb6844841ea64a8ad95325f9
Fernando Aguilar <ferag@live.com>
2019-02-20 16:54:56 +0100

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+sudo chown -R jovyan:users /home/jovyan/.local/share/jupyter
+sudo chown -R jovyan:users /home/jovyan/datasets
+su jovyan -c 'oneclient --authentication token --no_check_certificate /home/jovyan/datasets'
+tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
